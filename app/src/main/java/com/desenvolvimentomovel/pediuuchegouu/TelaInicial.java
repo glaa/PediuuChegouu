bcb152981bf0ae4299522dc05719026c9b31dda1
Thymoteo <tds.i@hotmail.com>
2019-10-03 07:50:48 -0300

    Tela inicial pronta!

--- app/src/main/java/com/desenvolvimentomovel/pediuuchegouu/TelaInicial.java
+++ app/src/main/java/com/desenvolvimentomovel/pediuuchegouu/TelaInicial.java
@@ -3,8 +3,11 @@ package com.desenvolvimentomovel.pediuuchegouu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.color.black;
+
+public class TelaInicial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
