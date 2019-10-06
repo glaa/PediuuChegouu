4931a366cff2e7d28876a94e96a95086854e280f
Thymoteo <tds.i@hotmail.com>
2019-10-06 18:40:03 -0300

    Tela de alterar dados e login!

---
+++ app/src/main/java/com/desenvolvimentomovel/pediuuchegouu/AlterarDados.java
@@ -0,0 +1,14 @@
+package com.desenvolvimentomovel.pediuuchegouu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlterarDados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_dados);
+    }
+}
