0987f92e3db23229785f1cebdcf6228da5dbc978
Gleison A. Araujo <gleisonaaraujo@gmail.com>
2019-11-12 22:07:26 -0300

    Modificado a tela de login com algumas regras de acesso, mudança no navegador para diferenciar usuario logado do deslogado.

--- app/src/main/java/com/desenvolvimentomovel/pediuuchegouu/InicialActivity.java
+++ app/src/main/java/com/desenvolvimentomovel/pediuuchegouu/InicialActivity.java
@@ -1,7 +1,9 @@
 package com.desenvolvimentomovel.pediuuchegouu;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -14,31 +16,59 @@ import androidx.appcompat.widget.Toolbar;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
 
 public class InicialActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private Button btCardapio;
+    public static SharedPreferences preferencias;
+    private boolean usuarioLogado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
 
+        preferencias = getSharedPreferences("preferencias_usuario",MODE_PRIVATE);
+
+        usuarioLogado = verificarUsuarioLogado();
+
+        if(usuarioLogado){
+            setContentView(R.layout.activity_inicial);
+            Log.d("INIacti", "foi");
+
+        } else {
+            Log.d("INIacti","delogado 1");
+
+            setContentView(R.layout.activity_inicial_deslogado);
+        }
+
+
         //Incluindo o toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
         //Definindo o layout do menu lateral
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        DrawerLayout drawer;
+        if(usuarioLogado){
+            drawer = findViewById(R.id.drawer_layout);
+        } else {
+            drawer = findViewById(R.id.drawer_layout_deslogado);
+        }
+
         //Inserindo o botão menu no toolbar
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
         //Definindo o conteiner do menu lateral
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        NavigationView navigationView;
+        if(usuarioLogado){
+            navigationView = findViewById(R.id.nav_view);
+        } else {
+            navigationView = findViewById(R.id.nav_view_deslogado);
+        }
         navigationView.setNavigationItemSelectedListener(this);
 
         btCardapio = findViewById(R.id.buttonCardapio);
@@ -55,7 +85,13 @@ public class InicialActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        DrawerLayout drawer;
+        if(usuarioLogado){
+            drawer = findViewById(R.id.drawer_layout);
+        } else {
+            drawer = findViewById(R.id.drawer_layout_deslogado);
+        }
+
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -79,6 +115,10 @@ public class InicialActivity extends AppCompatActivity
                 break;
             case R.id.nav_sair:
                 Toast.makeText(this,"Sair",Toast.LENGTH_SHORT).show();
+                apagarUsuario();
+                intent = new Intent(this, InicialActivity.class);
+                startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                //setContentView(R.layout.activity_inicial_deslogado);
                 break;
             case R.id.nav_alterar_dados:
                 intent = new Intent(this, AlterarDados.class);
@@ -88,14 +128,51 @@ public class InicialActivity extends AppCompatActivity
                 intent = new Intent(this, AlterarSenha.class);
                 startActivity(intent);
                 break;
+            case R.id.nav_entrar:
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_cadastro:
+                intent = new Intent(this, TelaCadastro.class);
+                startActivity(intent);
+                break;
             default:
                 super.onOptionsItemSelected(item);
         }
+        DrawerLayout drawer;
+        if(usuarioLogado){
+            drawer = findViewById(R.id.drawer_layout);
+        } else {
+            drawer = findViewById(R.id.drawer_layout_deslogado);
+        }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    private boolean verificarUsuarioLogado(){
+        boolean logado = true;
+        if(!preferencias.contains("logado")){
+            logado = false;
+        }
+        return logado;
+    }
 
+    private void mudarBotao(Button botao, String texto){
+        botao.setText(texto);
+    }
+
+    public static void salvarUsuario(String telefone){
+        SharedPreferences.Editor editor = InicialActivity.preferencias.edit();
+        editor.putString("telefone",telefone);
+        editor.putBoolean("logado",true);
+        editor.apply();
+    }
+
+    private void apagarUsuario(){
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.clear();
+        editor.apply();
+        Log.d("INIactia", "apagado");
+    }
 }
--- app/src/main/java/com/desenvolvimentomovel/pediuuchegouu/LoginActivity.java
+++ app/src/main/java/com/desenvolvimentomovel/pediuuchegouu/LoginActivity.java
@@ -1,18 +1,70 @@
 package com.desenvolvimentomovel.pediuuchegouu;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView tvTelefone;
+    private TextView tvSenha;
+    private Button btEntrar;
+    private Button btCadastro;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Login");
+        toolbar.setNavigationIcon(R.drawable.ic_action_arrow_left);
+        setSupportActionBar(toolbar);
+
+        tvTelefone = findViewById(R.id.tv_telefone_login);
+        tvSenha = findViewById(R.id.tv_senha_login);
+        btEntrar = findViewById(R.id.bt_entrar_login);
+        btCadastro = findViewById(R.id.bt_cadastro_login);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tvTelefone.getText().toString().length() == 0){
+                    tvTelefone.setError("Obrigatório o preenchimento do telefone");
+                    tvTelefone.requestFocus();
+                    return;
+                }
+
+                if(tvSenha.getText().toString().length() == 0){
+                    tvSenha.setError("Obrigatório o preenchimento da senha");
+                    tvSenha.requestFocus();
+                    return;
+                }
+
+                String telefone = tvTelefone.getText().toString();
+                String senha = tvSenha.getText().toString();
+
+                //conferirDados(telefone, senha);
+
+                InicialActivity.salvarUsuario(telefone);
+
+                Intent intent = new Intent(LoginActivity.this,InicialActivity.class);
+                startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
